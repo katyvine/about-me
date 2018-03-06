51a1c1808737bb640b023ef360542b4fa97545f2
Katy Bowers <katyvine@gmail.com>
2018-03-06 15:57:26 -0800

    updating JS file to be yes or no questions, not free form

--- js/app.js
+++ js/app.js
@@ -2,52 +2,48 @@
 
 // IF ESLSE
 
-var homeTown = prompt('Where am I from?').toLowerCase();
+var homeTown = prompt('Am I from Seattle? (yes or no?)').toLowerCase();
 
-if (homeTown === 'seattle'){
+if (homeTown === 'yes'){
   alert('Yes! You\'re right!');
 } else {
   alert('Nope! I\'m from Seattle, the best city to live in.');
 }
 
-console.log('Home town guess: ' + homeTown);
+console.log('Am I from Seattle?: ' + homeTown);
 
-var favoriteTeam = prompt('What is my favorite Seattle sports team?').toLowerCase();
+var favoriteTeam = prompt('Is my favorite Seattle Sports team the Seahawks? (yes or no?)').toLowerCase();
 
-if (favoriteTeam === 'seahawks'){
-  alert('Yes, and you also would have been right if you guessed the Sounders or all Husky sports.');
-} else if (favoriteTeam === 'sounders'){
-  alert('Yes, and you also would have been right if you guessed the Seahawks or all Husky sports.');
-} else if (favoriteTeam === 'huskies'){
-  alert('Yes, and you also would have been right if you guessed the Seahawks or Sounders.');
-} else { alert('Good guess, but no.');
+if (favoriteTeam === 'yes'){
+  alert('Yes, and you also would have been right if I had asked about the Sounders or all Husky sports.');
+} else { alert('I do love Seahawks, but also the Sounders and all Husky sports');
 }
 
-console.log('Favorite team guess: ' + favoriteTeam);
+console.log('Favorite team is Seahawks?: ' + favoriteTeam);
 
-var beerChoice = prompt('I live in Ballard, what\'s my favorite Ballard Brewery?').toLowerCase();
+var beerChoice = prompt('I live in Ballard, is my favorite Ballard Brewery Stoup? (yes or no?)').toLowerCase();
 
-if (beerChoice === 'ruben\'s'){
-  alert('Nailed it!');
+if (beerChoice === 'no'){
+  alert('Nailed it!I like Ruben\'s the best.');
 } else {alert('Nope, I like Ruben\'s more.');
 }
 
-console.log('Ballard Brewery guess: ' + beerChoice);
+console.log('Do I like Stoup?: ' + beerChoice);
 
-var missingContinent = prompt('I have been to 5 of 7 continents. Can you guess one of the 2 I haven\'t been to?').toLowerCase();
+var missingContinent = prompt('I have been to 5 of 7 continents. Is Australia one that I am missing? (yes or no?)').toLowerCase();
 
-if (missingContinent === 'asia'|| missingContinent === 'antartica'){
-  alert('You got one!');
-} else { alert('try again');
+if (missingContinent === 'no'){
+  alert('You got it! I have been there, but not to Asia or Antartica');
+} else { alert('Try again, I haven\'t been to Asia or Antartica');
 }
 
-console.log('One of my two missing continent guesses: ' + missingContinent);
+console.log('Have I been to Antartica?: ' + missingContinent);
 
-var schoolQuestion = prompt('Where did I go to college? Please type the full name, no abbreviation.').toLowerCase();
+var schoolQuestion = prompt('Did I go to the University of Washington? (yes or no)').toLowerCase();
 
-if (schoolQuestion === 'university of washington'){
+if (schoolQuestion === 'yes'){
   alert('Go Dawgs!');
-} else {alert('Nope, I gave all my money to the University of Washington. Woof!');
+} else {alert('Nope, I did give all my money to the University of Washington. Woof!');
 }
 
-console.log('Where I went to school guess: ' + schoolQuestion);
+console.log('Did I go to UW?: ' + schoolQuestion);
