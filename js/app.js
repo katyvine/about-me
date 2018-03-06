6277c797eb927a29f4155f458d6717136cdfeca1
Katy Bowers <katyvine@gmail.com>
2018-03-06 15:02:44 -0800

    File update with console log code

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ if (homeTown === 'seattle'){
   alert('Nope! I\'m from Seattle, the best city to live in.');
 }
 
+console.log('Home town guess: ' + homeTown);
+
 var favoriteTeam = prompt('What is my favorite Seattle sports team?').toLowerCase();
 
 if (favoriteTeam === 'seahawks'){
@@ -21,12 +23,16 @@ if (favoriteTeam === 'seahawks'){
 } else { alert('Good guess, but no.');
 }
 
+console.log('Favorite team guess: ' + favoriteTeam);
+
 var beerChoice = prompt('I live in Ballard, what\'s my favorite Ballard Brewery?').toLowerCase();
 
 if (beerChoice === 'ruben\'s'){
   alert('Nailed it!');
 } else {alert('Nope, I like Ruben\'s more.');
 }
+
+console.log('Ballard Brewery guess: ' + beerChoice);
 
 var missingContinent = prompt('I have been to 5 of 7 continents. Can you guess one of the 2 I haven\'t been to?').toLowerCase();
 
@@ -35,9 +41,13 @@ if (missingContinent === 'asia'|| missingContinent === 'antartica'){
 } else { alert('try again');
 }
 
+console.log('One of my two missing continent guesses: ' + missingContinent);
+
 var schoolQuestion = prompt('Where did I go to college? Please type the full name, no abbreviation.').toLowerCase();
 
 if (schoolQuestion === 'university of washington'){
   alert('Go Dawgs!');
 } else {alert('Nope, I gave all my money to the University of Washington. Woof!');
 }
+
+console.log('Where I went to school guess: ' + schoolQuestion);
