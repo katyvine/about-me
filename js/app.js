cb0f62188121127ba959d308ac3a6c0904622bf8
Justin Morris <justinjodymorris@gmail.com>
2018-03-08 13:53:00 -0800

    functions added to 1-6

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 'use strict';
 // Into
+
+var userName;
+
 function userNameFunction() {
-  var userName = prompt('Welcome! What is your name?');
+  userName = prompt('Welcome! What is your name?');
   console.log (userName);
 
   alert('Hi '+ userName + '! Are you ready to play my guessing game? It\'s about me!');
+  return userName;
 }
 
 userNameFunction();
@@ -28,7 +32,7 @@ function homeTownFunction() {
 
 homeTownFunction();
 
-function petQuestion() {
+function petQuestionFunction() {
   var petQuestion = prompt('Do I have any pets? (yes or no?)').toLowerCase();
 
   if (petQuestion === 'no'){
@@ -41,71 +45,85 @@ function petQuestion() {
   console.log('Do I have pets?: ' + petQuestion);
 }
 
-petQuestion();
+petQuestionFunction();
 
-var beerChoice = prompt('I live in Ballard, is Stoup my favorite Ballard Brewery? (yes or no?)').toLowerCase();
+function beerChoiceFunction() {
+  var beerChoice = prompt('I live in Ballard, is Stoup my favorite Ballard Brewery? (yes or no?)').toLowerCase();
 
-if (beerChoice === 'no'){
-  alert('Nailed it! I like Reuben\'s the best.');
-  score ++;
-  console.log ('Q3 score:' + score);
-} else {
-  alert('Nope, I like Reuben\'s more.');
+  if (beerChoice === 'no'){
+    alert('Nailed it! I like Reuben\'s the best.');
+    score ++;
+    console.log ('Q3 score:' + score);
+  } else {
+    alert('Nope, I like Reuben\'s more.');
+  }
+
+  console.log('Do I like Stoup?: ' + beerChoice);
 }
 
-console.log('Do I like Stoup?: ' + beerChoice);
+beerChoiceFunction();
 
-var missingContinent = prompt('I have been to 5 of 7 continents. Is Australia one that I am missing? (yes or no?)').toLowerCase();
+function missingContinentFunction() {
+  var missingContinent = prompt('I have been to 5 of 7 continents. Is Australia one that I am missing? (yes or no?)').toLowerCase();
 
-if (missingContinent === 'no'){
-  alert('You got it! I have been there, but not to Asia or Antartica');
-  score ++;
-  console.log ('Q4 score:' + score);
-} else {
-  alert('Try again, I haven\'t been to Asia or Antartica');
+  if (missingContinent === 'no'){
+    alert('You got it! I have been there, but not to Asia or Antarctica');
+    score ++;
+    console.log ('Q4 score:' + score);
+  } else {
+    alert('Try again, I haven\'t been to Asia or Antarctica');
+  }
+
+  console.log('Have I been to Australia?: ' + missingContinent);
 }
 
-console.log('Have I been to Antartica?: ' + missingContinent);
+missingContinentFunction();
 
-var schoolQuestion = prompt('Did I go to the University of Washington? (yes or no)').toLowerCase();
+function schoolQuestionFunction() {
+  var schoolQuestion = prompt('Did I go to the University of Washington? (yes or no)').toLowerCase();
 
-if (schoolQuestion === 'yes'){
-  alert('Go Dawgs!');
-  score ++;
-  console.log ('Q5 score:' + score);
-} else {
-  alert('Nope, I did give all my money to the University of Washington. Woof!');
+  if (schoolQuestion === 'yes'){
+    alert('Go Dawgs!');
+    score ++;
+    console.log ('Q5 score:' + score);
+  } else {
+    alert('Nope, I did give all my money to the University of Washington. Woof!');
+  }
+
+  console.log('Did I go to UW?: ' + schoolQuestion);
 }
 
-console.log('Did I go to UW?: ' + schoolQuestion);
+schoolQuestionFunction();
 
 // //Question 6: Numeric Imput
+function countryGuessFunction() {
+  var countryGuessLeft = 4;
 
-var countryGuessLeft = 4;
+  while (countryGuessLeft > 0){
+    var countryGuess = prompt('I spent 17 months traveling, how many countries did I go to?');
+    console.log (countryGuess);
 
-while (countryGuessLeft > 0){
-  var countryGuess = prompt('I spent 17 months traveling, how many countries did I go to?');
-  console.log (countryGuess);
+    if (parseInt(countryGuess) === 29) {
+      alert('You got it!! Wow!');
+      score ++;
+      console.log ('Q6 score:' + score);
+      countryGuessLeft = 0;
+      break;
+    } else if (parseInt(countryGuess) > 29) {
+      alert('I went to a lot, but not that many! Try again');
+      countryGuessLeft --;
+    } else if (parseInt(countryGuess) < 29) {
+      alert ('That\'s quite a few, but I went to even more! Try again.');
+      countryGuessLeft --;
+    }
 
-  if (parseInt(countryGuess) === 29) {
-    alert('You got it!! Wow!');
-    score ++;
-    console.log ('Q6 score:' + score);
-    countryGuessLeft = 0;
-    break;
-  } else if (parseInt(countryGuess) > 29) {
-    alert('I went to a lot, but not that many! Try again');
-    countryGuessLeft --;
-  } else if (parseInt(countryGuess) < 29) {
-    alert ('That\'s quite a few, but I went to even more! Try again.');
-    countryGuessLeft --;
-  }
-
-  if (countryGuessLeft === 0) {
-    alert('Sorry you ran out of guesses. I went to 29 countries!');
+    if (countryGuessLeft === 0) {
+      alert('Sorry you ran out of guesses. I went to 29 countries!');
+    }
   }
 }
 
+countryGuessFunction();
 
 //Question 7: Mulitple Correct Answers
 
@@ -115,7 +133,6 @@ var foodGuessLeft = 6;
 
 while (foodGuessLeft > 0 ){
   var foodGuess = prompt('Can you guess what some of my favorite foods are?').toLowerCase();
-  console.log(foodGuessLeft, 'at top of while loop');
 
   for(var i = 0; i < favoriteFoodsArray.length; i++){
     console.log(foodGuess);
@@ -135,7 +152,6 @@ while (foodGuessLeft > 0 ){
   if (foodGuessLeft === 0){
     alert ('You are out of guesses sorry. My favorites are: chocolate, cheese, chocolate chip cookies and wine (yes wine is a food!)');
   }
-  console.log(foodGuessLeft, 'at bottom of while loop');
 }
 
 alert (userName + ', I hope you enjoyed that. You answered ' + score + ' of 7 questions correctly. Please play again soon.');
